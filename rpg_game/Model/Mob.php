fb7a08c1e96ed931aa8b03613bd02888bae04e55
Loïc LECOMTE <l.lecomte77@gmail.com>
2021-03-17 16:19:43 +0100

    rpg_game

---
+++ rpg_game/Model/Mob.php
@@ -0,0 +1,9 @@
+<?php
+namespace Model;
+
+abstract class Mob extends Character
+{
+    /**@var string */
+    protected const SYMBOL = '&#127947;&#127995;';
+    
+}
---
+++ rpg_game/Model/Warrior.php
@@ -0,0 +1,13 @@
+<?php
+namespace Model;
+
+
+/**
+ * Warrior
+ */
+final class Warrior extends Character
+{
+    /**@var string */
+    protected const SYMBOL = '&#127947;&#127995;';
+
+}
